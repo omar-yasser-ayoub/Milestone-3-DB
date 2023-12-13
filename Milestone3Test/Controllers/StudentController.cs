385007f4f489bf9844eb9b2062504203d1cad121
Farah065 <farahahmad065@gmail.com>
2023-12-13 17:54:49 +0200

    Syncing up code

--- Milestone3Test/Controllers/StudentController.cs
+++ Milestone3Test/Controllers/StudentController.cs
@@ -17,52 +17,53 @@ namespace Milestone3Test.Controllers
         }
         [HttpPost]
         [Route("ChooseInstructor")]
-        public IActionResult ChooseInstructor()
+        public IActionResult ChooseInstructor([FromHeader] string StudentID, [FromHeader] string instructorID, [FromHeader] string CourseID, [FromHeader] string current_semester_code)
         {
-            // TODO code here
-            return null;
+            _dbContext.Procedures_StudentChooseInstructor(StudentID, instructorID, CourseID, current_semester_code);
+
+            return StatusCode(StatusCodes.Status200OK);
         }
         [HttpPost]
         [Route("AddMobile")]
-        public IActionResult AddMobile()
+        public IActionResult AddMobile([FromHeader] string StudentID, [FromHeader] string mobile_number)
         {
-            // TODO code here
-            return null;
+            _dbContext.Procedures_StudentAddMobile(StudentID, mobile_number);
+            return StatusCode(StatusCodes.Status200OK);
         }
         [HttpPost]
         [Route("RegisterFirstMakeup")]
-        public IActionResult RegisterFirstMakeup()
+        public IActionResult RegisterFirstMakeup([FromHeader] string StudentID, [FromHeader] string courseID, [FromHeader] string studentCurr_sem)
         {
-            // TODO code here
-            return null;
+            _dbContext.Procedures_StudentRegisterFirstMakeup(StudentID, courseID, studentCurr_sem);
+            return StatusCode(StatusCodes.Status200OK);
         }
         [HttpPost]
         [Route("RegisterSecondMakeup")]
-        public IActionResult RegisterSecondMakeup()
+        public IActionResult RegisterSecondMakeup([FromHeader] string StudentID, [FromHeader] string courseID, [FromHeader] string studentCurr_sem)
         {
-            // TODO code here
-            return null;
+            _dbContext.Procedures_StudentRegisterSecondMakeup(StudentID, courseID, studentCurr_sem);
+            return StatusCode(StatusCodes.Status200OK);
         }
         [HttpPost]
         [Route("Registration")]
-        public IActionResult Registration()
+        public IActionResult Registration([FromHeader] string first_name, [FromHeader] string last_name, [FromHeader] string password, [FromHeader] string faculty, [FromHeader] string email, [FromHeader] string major, [FromHeader] string Semester)
         {
-            // TODO code here
-            return null;
+            int Student_id = _dbContext.Procedures_StudentRegistration(first_name, last_name, password, faculty, email, major, Semester);
+            return StatusCode(StatusCodes.Status200OK, new { StudentId = Student_id });
         }
         [HttpPost]
         [Route("SendingCHRequest")]
-        public IActionResult SendingCHRequest()
+        public IActionResult SendingCHRequest([FromHeader] string StudentID, [FromHeader] string credit_hours, [FromHeader] string type, [FromHeader] string comment)
         {
-            // TODO code here
-            return null;
+            _dbContext.Procedures_StudentSendingCHRequest(StudentID, credit_hours, type, comment);
+            return StatusCode(StatusCodes.Status200OK);
         }
         [HttpPost]
         [Route("SendingCourseRequest")]
-        public IActionResult SendingCourseRequest()
+        public IActionResult SendingCourseRequest([FromHeader] string courseID, [FromHeader] string StudentID, [FromHeader] string type, [FromHeader] string comment)
         {
-            // TODO code here
-            return null;
+            _dbContext.Procedures_StudentSendingCourseRequest(courseID, StudentID, type, comment);
+            return StatusCode(StatusCodes.Status200OK);
         }
         [HttpPost]
         [Route("ViewMS")]
