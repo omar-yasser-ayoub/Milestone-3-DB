a11f1f786b10f76cb8e1f9803fb7ac91a9e1382c
reeka <omar-yasser-ayoub@student.guc.edu.eg>
2023-12-13 23:48:10 +0200

    Added Course And Request + COurse Dropdown

---
+++ Milestone3Test/ClientApp/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom'
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <div className="container" style={{
+            background: `linear-gradient(90deg, #4F4381, #8D81C7)`,
+            color: '#fff',
+        }}>
+            <h1>test</h1>
+        </div>
+    );
+};
+
+export default Navbar;
+
+
