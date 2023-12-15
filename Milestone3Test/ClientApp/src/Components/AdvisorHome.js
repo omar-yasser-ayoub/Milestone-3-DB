2991fc44b8b32f1ada479a862587879bce9f5055
Farah065 <farahahmad065@gmail.com>
2023-12-15 13:08:45 +0200

    Merging (again)

--- Milestone3Test/ClientApp/src/Components/AdvisorHome.js
+++ Milestone3Test/ClientApp/src/Components/AdvisorHome.js
@@ -15,38 +15,7 @@ const divStyle = {
 const HomeNav = (props) => {
     return (
         <div>
-            <BrowserRouter>
-                <div>
-                    <nav className="nav">
-                        <div>
-                            <Link to="/advisor/students" className="nav-item">
-                                <div style={divStyle}>
-                                    <h2>Students</h2>
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/advisor/graduation-plans" className="nav-item">
-                                <div style={divStyle}>
-                                    <h2>Graduation Plans</h2>
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/advisor/requests" className="nav-item">
-                                <div style={divStyle}>
-                                    <h2>Requests</h2>
-                                </div>
-                            </Link>
-                        </div>
-                    </nav>
-                    <Routes>
-                        <Route path="/advisor/students" element={<AdvisorStudents />} />
-                        <Route path="/advisor/graduation-plans" element={<AdvisorGradPlans />} />
-                        <Route path="/advisor/requests" element={<AdvisorRequests />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
+            
         </div>
     );
 };
